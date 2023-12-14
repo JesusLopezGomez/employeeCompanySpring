d5f30652e2c4fb1f2deaf55844c4aad97fe915b7
JesuLopezGomez <jelopez.gom@gmail.com>
2023-12-14 10:06:36 +0100

    .

---
+++ src/main/java/com/jacaranda/employeeCompany/repository/CompanyRepository.java
@@ -0,0 +1,13 @@
+package com.jacaranda.employeeCompany.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jacaranda.employeeCompany.model.Company;
+
+
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
+
+	
+}
